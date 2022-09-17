35df4717b726fdb782cc72663e8bf732bb40424d
oguzhanaslann <oguz21199@gmail.com>
2022-09-17 18:41:59 +0300

    Initial Commit

---
+++ FitTrack_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitTrack_IOS
+//
+//  Created by Oğuzhan Aslan on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
